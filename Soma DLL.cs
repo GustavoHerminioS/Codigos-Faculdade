81a77e757336344d120ac42fc8ca57c0bac7a247
GustavoHerminioS <herminiogustavo000@gmail.com>
2025-02-28 20:26:08 -0300

    Add files via upload

---
+++ Soma DLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calc
+    {
+        public int Soma(int x, int y)
+        {
+            return x + y; 
+        }
+         public int Multiplicacao(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divisao (int x, int y)
+        {
+            return x / y;
+
+        }
+
+        public int Subtracao (int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
