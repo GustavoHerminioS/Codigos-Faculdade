95909bc96aa84fb8e2fc41ee3c4c33f9f00a009f
GustavoHerminioS <herminiogustavo000@gmail.com>
2025-03-25 17:00:32 -0300

    Update C# Soma.cs

--- C# Soma.cs
+++ C# Soma.cs
@@ -1,4 +1,11 @@
-﻿using Calculadora;
+DISCLAIMER: Os códigos apresentados foram desenvolvidos com o intuito de preparação para uma avaliação
+utilizando os métodos e abordagens solicitados pelo professor. Eles não visam, de forma alguma, representar soluções
+finais ou definitivas, mas sim servir como um exercício de aprendizado e aplicação dos conceitos requeridos para a
+avaliação.
+Os sistemas solicitados estão comentados no código para que não sejam executadas, mas fiquem salvas no arquivo.
+Essa abordagem foi adotada para manter o histórico de todos os exercícios realizados.
+
+using Calculadora;
 using System;
 using System.Collections.Generic;
 using System.Linq;
